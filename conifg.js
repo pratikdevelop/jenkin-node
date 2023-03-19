74b9227101c772f4ceafc4b36495d0f397646bb3
Pratik_5678 <pratikraut88895@gmail.com>
2023-03-19 19:00:07 +0530

    remove npm install from jenkins stage

--- conifg.js
+++ conifg.js
@@ -1,14 +1,18 @@
 // const mongoose = require("mongoose");
 const { MongoClient } = require("mongodb");
 const NEW_MONGO_URL = process.env.MONGO_URL;
-console.log(NEW_MONGO_URL);
-const connectMongo = async () => {
+const connectMongo = () => {
   try {
     const client = new MongoClient(NEW_MONGO_URL);
-    return client;
-  } catch (e) {
-    return e;
+    // MongoClient.connect(NEW_MONGO_URL)
+    const Moviesdata = data.db('sample_mflix').collection('movies')
+    return Moviesdata;
   }
+  catch (error) {
+    return error
+  }
+
+
 };
 
 const database = connectMongo();
--- server.js
+++ server.js
@@ -1,17 +1,30 @@
 const  express  = require( 'express')
 const app = express();
 const port = process.env.PORT || 3000;
+const { MongoClient } = require("mongodb");
 const path = require('path')
 require("dotenv").config({ path: path.join(__dirname, "./.env") });
+const { createProxyMiddleware } = require("http-proxy-middleware");
+const cors = require("cors");
+const NEW_MONGO_URL = process.env.MONGO_URL;
+const client = new MongoClient(NEW_MONGO_URL);
+const Moviesdata = client.db('sample_mflix').collection('movies');
 
-const  {database}  =  require('./conifg')
-app.get('/',(req,res)=>{
-    let data = []
-    database.then((response )=>{
-        data.push(response)
-    })
-    console.log(data)
-    res.json(data)
+app.use(cors());
+app.use(express.json());
+app.use(
+  "/api",
+  createProxyMiddleware({ target: "https://chat-api-ye6d.onrender.com/", changeOrigin: true })
+);
+express.urlencoded({ extended: false });
+
+
+
+const  database  =  require('./conifg')
+app.get('/',async(req,res)=>{
+    const datta = await Moviesdata.find().limit(20);
+    const re= await datta.toArray()
+res.sendFile(path.join(__dirname+ '/index.html'))
 })
 app.listen(port, ()=>{
     console.log(`port listening on http://localhost:${port}`);
